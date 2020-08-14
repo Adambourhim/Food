386b867db94d7d466cdb93710336dbc63fe4c884
Adambourhim <47826357+Adambourhim@users.noreply.github.com>
2020-08-14 15:50:20 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useEffect , useState} from 'react';
 import './App.css';
 
 function App() {
+ 
+
+const APP_ID = "33f9816c" ;
+const APP_KEY = "c0868fa8eeb658ee8638dfdfbf70cf2e" ;
+//const ExempleReq = `https://api.edamam.com/api/nutrition-details?app_id=${33f9816c}&app_key=${c0868fa8eeb658ee8638dfdfbf70cf2e}`;
+
+const [info,setInfo] = useState ([]) ;
+const [loading,setLoading] = useState (true) ;
+
+
+useEffect ( () => {
+  corona();
+},[]) ;
+
+const corona = async () => {
+  const response = await fetch ("https://api.edamam.com/api/nutrition-details?app_id=46c62328c&app_key=2a7ff77d23cea975370283ca1ef3cab9");
+  const data = await response.json();
+  
+  console.log(data);
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
     </div>
   );
 }
---
+++ src/Cases.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Cases = ({title}) =>{
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default Cases ;
